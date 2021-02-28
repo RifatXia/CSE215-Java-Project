b5c3bbf5adfa50f2af7164b535c1114396ff8d3f
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2021-02-28 21:37:58 +0600

    Delete Method.java

--- Method.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-public interface Method 
-{
-	public abstract void printFacultyInfo() throws Exception;
-	public abstract void printStudentInfo() throws Exception;
-	public abstract void FileReader() throws Exception;
-	public abstract void getAttendance();
-}
