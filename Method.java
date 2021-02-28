1891a97d7bf56f0d3b23ee3faa3071cdffa91744
NafisaProme <76622425+NafisaProme@users.noreply.github.com>
2021-02-28 21:45:09 +0600

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,9 @@
+package project;
+
+public interface Method 
+{
+	public abstract void printFacultyInfo() throws Exception;
+	public abstract void printStudentInfo() throws Exception;
+	public abstract void FileReader() throws Exception;
+	public abstract void getAttendance();
+}
