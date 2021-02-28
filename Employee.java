7df3f2afa96f2522a468b6a0b595e2016eab800b
NafisaProme <76622425+NafisaProme@users.noreply.github.com>
2021-02-28 21:43:36 +0600

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,25 @@
+package project;
+
+public class Employee extends Person {
+	private double salary;
+	
+	public Employee() 
+	{
+		
+	}
+	public Employee(String n, String a, String e, String p, double s) {
+		this.salary=s;
+		
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public String toString() {
+		return "Employee";
+	}
+
+}
